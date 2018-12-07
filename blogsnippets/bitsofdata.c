ff440059f9cb3068a2ae2b2bd6da3e63b1445549
Aric Maddux <aricmaddux@Arics-MBP.attlocal.net>
2018-12-06 20:47:49 -0500

    adding c example to blog

---
+++ blogsnippets/bitsofdata.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stddef.h>
+
+void printBits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+
+    for (i=size-1;i>=0;i--)
+    {
+        for (j=7;j>=0;j--)
+        {
+            byte = (b[i] >> j) & 1;
+            printf("%u", byte);
+        }
+    }
+    puts("");
+}
+
+
+int main(int argv, char* argc[])
+{
+    int x = -4;
+    printBits(sizeof(x), &x);
+    return 0;
+}
