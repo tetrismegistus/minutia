fbc31e8bc635c9b5fff3ec7ca3fbfdfc3523ec60
Maddux <AMaddux@citizensenergygroup.com>
2019-03-21 14:09:00 -0400

    Front page HTML now generated from a template

--- exercises/obey_the_goat/functional_tests.py
+++ exercises/obey_the_goat/functional_tests.py
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         # She is invited to senter a to-do item straight away
-        inputbox = self.browseer.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
                 inputbox.get_attribute('placeholder'),
                 'Enter a to-do item'
@@ -41,7 +41,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows)
+                any(row.text == '1: Buy peacock feathers' for row in rows),
+                "New to-do item did not appear in table"
         )
 
 
