0d9f95cd49aae945bb115c1d4527d02af6a0ef37
Aric Maddux <madducks@gmail.com>
2020-03-22 15:03:41 -0400

    updated to color death age and scale the color

--- exercises/python_doodles/datamosher/conwayizer.py
+++ exercises/python_doodles/datamosher/conwayizer.py
@@ -1,3 +1,6 @@
+from enum import Enum
+import random
+
 import numpy as np
 from PIL import Image
 from scipy.ndimage import convolve
@@ -5,10 +8,54 @@ from scipy.ndimage import convolve
 from animation import Animation
 
 
-def conwayize(pil_img):
-    img_array = np.asarray(pil_img, dtype=np.int8)
+class State(Enum):
+    ALIVE = 1
+    DEAD = 2
+
+
+class Cell:
+    SCALE = {0: (0, 0, 0),
+             1: (155, 61, 17),
+             2: (177, 70, 20),
+             3: (200, 80, 24),
+             4: (222, 89, 28),
+             5: (223, 103, 42),
+             6: (225, 117, 61),
+             7: (227, 134, 84),
+             8: (255, 0, 0)}
+
+    def __init__(self, x, y, state):
+        self.x = x
+        self.y = y
+        self.fetidness = 8
+        self.state = state
+
+    @property
+    def color(self):
+        if self.state == State.ALIVE:
+            return 255, 0, 0
+        else:
+            return Cell.SCALE[self.fetidness]
+
+    def decay(self):
+        if self.fetidness > 0:
+            self.fetidness = (self.fetidness - 1) % len(self.SCALE)
+
+
+class Universe:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.cell_matrix = [[Cell(x, y, random.choice([State.ALIVE, State.DEAD, State.DEAD]))
+                            for x in range(width)]
+                            for y in range(height)]
+
+
+def conwayize(img_array, universe):
+    h, w = img_array.shape
 
     img_copy = np.copy(img_array)
+    new_image_array = Image.new('RGB', (w, h))
     kernel = np.array([[1, 1, 1],
                        [1, 0, 1],
                        [1, 1, 1]])
@@ -28,21 +75,31 @@ def conwayize(pil_img):
 
             if rule1 or rule2:
                 img_copy[idx][idy] = 1
+                universe.cell_matrix[idx][idy].state = State.ALIVE
+                universe.cell_matrix[idx][idy].fetidness = 8
             else:
                 img_copy[idx][idy] = 0
+                if universe.cell_matrix[idx][idy].state == State.DEAD:
+                    universe.cell_matrix[idx][idy].decay()
+                else:
+                    universe.cell_matrix[idx][idy].state = State.DEAD
 
-    return Image.fromarray(img_copy.astype(np.bool))
+            new_image_array.putpixel((idy - 1, idx - 1), universe.cell_matrix[idx][idy].color)
+
+    return new_image_array, img_copy
 
 
 def main():
     pil_img = Image.open('drump.jpg')
     image_pil_conv = pil_img.convert('1', dither=Image.FLOYDSTEINBERG)
-    iterations = 300
+    one_bit_array = np.asarray(image_pil_conv, dtype=np.int8)
+    universe = Universe(image_pil_conv.width, image_pil_conv.height)
+    iterations = 500
     gif = Animation('drump.gif', duration=.25)
     gif.frames = pil_img
     gif.frames = image_pil_conv
     for x in range(iterations):
-        image_pil_conv = conwayize(image_pil_conv)
+        image_pil_conv, one_bit_array = conwayize(one_bit_array, universe)
         gif.frames = image_pil_conv
 
     gif.save_gif()
@@ -50,4 +107,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
