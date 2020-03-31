ba3c50ff22b8defa81d66f5ad536b620c18747df
Aric Maddux <madducks@gmail.com>
2020-03-31 09:57:20 -0400

    animation, assignment cleanup
    added animation capability and cleaned up a few static assignments

--- exercises/python_doodles/datamosher/elemca.py
+++ exercises/python_doodles/datamosher/elemca.py
@@ -1,20 +1,25 @@
 from PIL import Image, ImageDraw
+from random import choice
+
+from animation import Animation
 
 
 class elemCa:
-    def __init__(self, ruleset=30, filename='ca.png', bg=(0, 0, 0), fg=(255, 255, 255), imw=200, imh=200):
+    def __init__(self, ruleset=30, filename=None, bg=(96, 167, 169), fg=(255, 255, 255), imw=200, imh=200,
+                 animation=None):
         self.bg = bg
         self.fg = fg
         self.imw = imw
         self.imh = imh
-        self.ca_img = Image.new('RGB', (imw, imh))
+        self.ca_img = Image.new('RGB', (imw, imh), color=bg)
         self.drawing = ImageDraw.Draw(self.ca_img)
         self.w = 2
-        self.rows = 100
-        self.cols = 100
+        self.rows = imh
+        self.cols = imw
         self.cells = []
         self.ruleset = [int(i) for i in format(ruleset, '08b')]
         self.filename = filename
+        self.animation = animation
         self.generate()
         self.draw()
 
@@ -22,11 +27,13 @@ class elemCa:
         return self.ruleset[7 - (4 * a + 2 * b + c)]
 
     def generate(self):
+
         for r in range(self.rows):
             self.cells.append([])
             for c in range(self.cols):
-                self.cells[r].append(0)
+                self.cells[r].append(choice([0, 0, 0, 0, 1]))
         self.cells[0][self.cols // 2] = 1
+        self.cells[0][0] = 1
 
         for i, row in enumerate(self.cells):
             for j in range(1, len(row) - 1):
@@ -48,7 +55,16 @@ class elemCa:
 
                 bbox = [x, y, x + self.w, y + self.w]
                 self.drawing.rectangle(bbox, f)
-        self.ca_img.save(self.filename)
+            if self.animation is not None:
+                self.animation.frames = self.ca_img.copy()
+
+        if self.animation is not None:
+            self.animation.save_gif()
+
+        if self.filename is not None:
+            self.ca_img.save(self.filename)
 
 
-elemCa(57, '57.png', (255, 128, 255))
+rule = 225
+elemCa(rule, f'{rule}.png', (255, 128, 255), (0, 0, 0), imw=500, imh=500,
+       animation=Animation(f'{rule}.gif', duration=.0001))
